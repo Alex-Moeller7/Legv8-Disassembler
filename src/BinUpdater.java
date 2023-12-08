6ce71a0ae59034e061e99db650fee35f031f04d8
Alex Moeller <alexmoe@iastate.edu>
2023-12-07 20:03:20 -0600

    file parser and BinUpdater

--- src/BinUpdater.java
+++ src/BinUpdater.java
@@ -4,38 +4,7 @@ import java.io.IOException;
 public class BinUpdater {
     public static void main(String[] args) {
         // Replace this with your desired binary value
-        String binaryValue = "100010110001111111111111010110000110100100011010010001101001000";
-        /* R, I, CB, B, D,
-         *
-         * EX: ADD (R type)
-         * 11 bit opcode - 10001011000
-         * 5 bit Rm
-         * 6 bit shamt
-         * 5 bit Rn
-         * 5 bit Rd
-         *
-         * EX: ADDI (I type)
-         * 10 bit opcode - 1001000100
-         * 12 bit ALU
-         * 5 bit Rn
-         * 5 bit Rd
-         *
-         * EX: LDUR (D type)
-         * 11 bit opcode - 11111000010
-         * 9 bit DT-address
-         * 2 bit op
-         * 5 bit Rn
-         * 5 bit Rd
-         *
-         * EX: BL (B type)
-         * 6 bit opcode - 100101
-         * 26 bit BR_Address
-         *
-         * EX: CBZ (CB type)
-         * 8 bit opcode - 10110100
-         * 19 bit COND_BR_Address
-         * 5 bit Rt
-         */
+        String binaryValue = "10001011000111110000000000010100";
 
         // Specify the file path
         String filePath = "src/test.bin";
--- src/FileParser.java
+++ src/FileParser.java
@@ -14,18 +14,83 @@ public class FileParser {
         try (InputStream is = new FileInputStream(fileLocation)) {
             int currentByte;
             while ((currentByte = is.read()) != -1) {
-                for (int i = 7; i >= 0; i--) {
+                //for (int i = 7; i >= 0; i--) {
+                for (int i = 0; i < 32; i++) {
                     int bit = (currentByte >> i) & 1;
                     bitList.add(bit);
 
                     // min is 6 bits
                     if (bitList.size() >= 6) {
                         Instruction matchedInstruction = findMatchingInstruction(bitList);
-                        if (matchedInstruction != null) {
-                            instructionList.add(matchedInstruction);
+                        if(matchedInstruction != null){
+                            System.out.println(matchedInstruction.getOpcode());
+                            System.out.println(bitList);
+                        }
 
-                            // clear for next opcode
-                            bitList.clear();
+                        if (matchedInstruction != null) {
+                            //we matched an instruction
+                            instructionList.add(matchedInstruction);
+                            String Mnem = matchedInstruction.getMnemonic();
+                            String instructionType = matchedInstruction.getFormat();
+                            System.out.println(Mnem);
+                            System.out.println(instructionType);
+
+                            switch(instructionType){
+                                /*
+                                EX: ADD (R type)
+                                    * 11 bit opcode - 10001011000
+                                    * 5 bit Rm
+                                    * 6 bit shamt
+                                    * 5 bit Rn
+                                    * 5 bit Rd
+                                 */
+                                case "R":
+
+                                    bitList.clear(); //clear bitlist to fill with new options
+                                    break;
+
+                                /*
+                                 * EX: ADDI (I type)
+                                 * 10 bit opcode - 1001000100
+                                 * 12 bit ALU
+                                 * 5 bit Rn
+                                 * 5 bit Rd
+                                 */
+                                case "I":
+                                    bitList.clear();
+                                    break;
+
+                                /* EX: BL (B type)
+                                 * 6 bit opcode - 100101
+                                 * 26 bit BR_Address
+                                 */
+                                case "B":
+                                    bitList.clear();
+                                    break;
+
+                                /* EX: LDUR (D type)
+                                 * 11 bit opcode - 11111000010
+                                 * 9 bit DT-address
+                                 * 2 bit op
+                                 * 5 bit Rn
+                                 * 5 bit Rd
+                                 */
+                                case "D":
+                                    bitList.clear();
+                                    break;
+
+                                /* EX: CBZ (CB type)
+                                 * 8 bit opcode - 10110100
+                                 * 19 bit COND_BR_Address
+                                 * 5 bit Rt
+                                 */
+                                case "CB":
+                                    bitList.clear();
+                                    break;
+
+                                default:
+                            }
+
                         }
                     }
                 }
