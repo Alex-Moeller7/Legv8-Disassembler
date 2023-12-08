3b9217f679b4ac6e2111664d155704fcd01a3c91
kevin21018212 <mmb2003@iastate.edu>
2023-12-08 14:19:05 -0600

    correct

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,8 @@ public class Main {
         if (args.length == 0) {
             System.err.println("No File Noe Hoes.");
         } else {
-            //while loop for constant reading of bytes here?
             List<Instruction> instructionList = FileParser.parseInstructions(args[0]);
-            //System.out.println(instructionList);
-            extractAndPrintMnemonics(instructionList);
-        }
-    }
 
-    private static void extractAndPrintMnemonics(List<Instruction> instructionList) {
-        for (Instruction instruction : instructionList) {
-            String mnemonic = instruction.getMnemonic();
-            //System.out.println("Mnemonic: " + mnemonic);
         }
     }
 }
